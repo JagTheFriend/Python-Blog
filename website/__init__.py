b7063b3925e1d07a51f641044df45216764de586
JagTheFriend <jagthefriend12@gmail.com>
2021-07-30 22:51:19 +0200

    Working on the database

--- website/__init__.py
+++ website/__init__.py
@@ -1,16 +1,53 @@
+from os import path
+import logging as log
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
 from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+log.basicConfig(
+    filename='app.log',
+    filemode='w',
+    format='%(name)s - %(levelname)s: %(message)s',
+    level=log.INFO
+)
 
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "HelloWorld"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)  # initialize the database
 
     from .views import views
     from .auth import auth
+
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
+    from .models import User
+    create_database(app=app)
+    login_manager = LoginManager()
+    # redirect the user to the login page
+    # if the user tries to visit a page without logging in
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id_):
+        """Allows to access the information about the user
+        with that specific user ID"""
+        return User.query.get(int(id_))
+
     return app
+
+
+def create_database(*, app: Flask):
+    """Creates the database if it doesn't already exists"""
+    if not path.exists(f"website/{DB_NAME}"):
+        db.create_all(app=app)
+        log.info(f"Created database called {DB_NAME}")
+    log.info(f"Database called {DB_NAME} already exists")
---
+++ website/models.py
@@ -0,0 +1,12 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class User(db.Model, UserMixin):
+    # unique ID of the user
+    id_ = db.Column(db.Integer, primary_key=True)
+    # max length of 150 characters, and it must be unique
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))  # doesn't need to be unique
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now)
