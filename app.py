59b6f9a8466669e1f744b18bdafa6f934916c479
JagTheFriend <jagthefriend12@gmail.com>
2021-07-30 22:49:30 +0200

    Added logger

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from website import create_app
+from website import create_app, log
 
 
 if __name__ == "__main__":
     app: Flask = create_app()
+    log.info("Starting the app")
     app.run(debug=True)
