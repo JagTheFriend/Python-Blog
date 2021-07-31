cee26f5c561ae32c283a241b41d2ec3de9019e07
JagTheFriend <jagthefriend12@gmail.com>
2021-07-31 13:38:28 +0200

    Fixing the `sqlalchemy.exc.OperationalError`

--- website/models.py
+++ website/models.py
@@ -5,8 +5,9 @@ from sqlalchemy.sql import func
 
 class User(db.Model, UserMixin):
     # unique ID of the user
-    id_ = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     # max length of 150 characters, and it must be unique
     email = db.Column(db.String(150), unique=True)
+    username = db.Column(db.String(150), unique=True)  # username
     password = db.Column(db.String(150))  # doesn't need to be unique
-    date_created = db.Column(db.DateTime(timezone=True), default=func.now)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
