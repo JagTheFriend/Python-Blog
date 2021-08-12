a0fcac8cdc0f169b0e1a377ed07ee9c0b39e7f90
JagTheFriend <jagthefriend12@gmail.com>
2021-08-12 21:06:41 +0200

    Updated index.js

--- website/static/index.js
+++ website/static/index.js
@@ -1,3 +1,22 @@
-function like(post_id){
-    console.log(`Post id: ${post_id}`);
+function like(postId) {
+    // gets the total number of likes
+    const likeCount = document.getElementById(`likes-count-${postId}`);
+    // the like button
+    const likeButton = document.getElementById(`like-button-${postId}`);
+
+    // sends a `POST` request
+    fetch(`/like-post/${postId}`, { method: "POST" })
+        .then((res) => res.json())
+        .then((data) => {
+            likeCount.innerHTML = data["likes"];
+            // user liked the post
+            if (data["liked"] === true) {
+                likeButton.className = "fas fa-thumbs-up";
+            // user didn't like the post
+            } else {
+                likeButton.className = "far fa-thumbs-up";
+            }
+        })
+        // something went wrong
+        .catch((e) => alert("Could not like post."));
 }
